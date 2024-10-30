9e1a57ca2c43e7b15283db6dfdc42c805e4edefc
modamo-gh <morganthemosaic@gmail.com>
2024-10-29 22:07:05 -0400

    Day 8 Part 1

--- 08/1.ts
+++ 08/1.ts
@@ -1,7 +1,9 @@
 import { readFileSync } from "fs";
 
 const instructions = readFileSync("input.txt", "utf8").trim().split("\n");
-const screen = Array.from({ length: 6 }, () => Array(50).fill("."));
+const SCREEN_WIDTH = 50
+const SCREEN_LENGTH = 6;
+const screen = Array.from({ length: SCREEN_LENGTH }, () => Array(SCREEN_WIDTH).fill("."));
 
 const createRect = (width: number, length: number) => {
 	for (let i = 0; i < width; i++) {
@@ -13,23 +15,23 @@ const createRect = (width: number, length: number) => {
 
 const rotateRow = (row: number, rotationAmount: number) => {
 	const rowClone = [...screen[row]];
-    const rowLength = screen[row].length;
+	const rowLength = screen[row].length;
 
 	for (let i = 0; i < rowLength; i++) {
-		screen[row][i] = rowClone[(i + rotationAmount) % rowLength];
+		screen[row][i] = rowClone[(i + (rowLength - rotationAmount)) % rowLength];
 	}
 };
 
 const rotateColumn = (column: number, rotationAmount: number) => {
 	const columnClone = [];
-	const rowLength = screen[0].length;
+	const columnLength = screen.length;
 
-	for (let i = 0; i < rowLength; i++) {
+	for (let i = 0; i < columnLength; i++) {
 		columnClone.push(screen[i][column]);
 	}
 
-	for (let i = 0; i < rowLength; i++) {
-		screen[i][column] = columnClone[(i + rotationAmount) % rowLength];
+	for (let i = 0; i < columnLength; i++) {
+		screen[i][column] = columnClone[(i + (columnLength - rotationAmount)) % columnLength];
 	}
 };
 
@@ -42,16 +44,28 @@ for (const instruction of instructions) {
 			.split("x")
 			.map((dimension) => parseInt(dimension));
 
-		createRect(width, length);
+		createRect(length, width);
 	} else if (action === "rotate" && tokens[1] === "row") {
-		const row = parseInt(tokens[2].split("x")[1]);
+		const row = parseInt(tokens[2].split("=")[1]);
 		const rotationAmount = parseInt(tokens[4]);
 
 		rotateRow(row, rotationAmount);
 	} else if (action === "rotate" && tokens[1] === "column") {
-		const column = parseInt(tokens[2].split("x")[1]);
+		const column = parseInt(tokens[2].split("=")[1]);
 		const rotationAmount = parseInt(tokens[4]);
 
 		rotateColumn(column, rotationAmount);
 	}
 }
+
+let numberOfLitPixels = 0;
+
+for(let i = 0; i < screen.length; i++){
+	for(let j = 0; j < screen[i].length; j++){
+		if(screen[i][j] === "#"){
+			numberOfLitPixels++;
+		}
+	}
+}
+
+console.log(numberOfLitPixels)
